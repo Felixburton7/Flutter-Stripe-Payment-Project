088b2ccb974f30c264375e0ccc140939a5deb282
Felixburton7 <felixburton2002@gmail.com>
2024-05-31 23:39:14 -0700

    Initial commit

---
+++ stripe-node-example/create_price.js
@@ -0,0 +1,20 @@
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
+
+stripe.products.create({
+  name: 'Starter Subscription',
+  description: '$12/Month subscription',
+}).then(product => {
+  stripe.prices.create({
+    unit_amount: 1200,
+    currency: 'usd',
+    recurring: {
+      interval: 'month',
+    },
+    product: product.id,
+  }).then(price => {
+    console.log('Success! Here is your starter subscription product id: ' + product.id);
+    console.log('Success! Here is your starter subscription price id: ' + price.id);
+  });
+}).catch(error => {
+  console.error('Error creating product or price:', error);
+});
